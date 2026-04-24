0fcd79bfd0567ede57d024f240bcf664a1f2d3cb
ayushdueby <115997065+ayushdubey@users.noreply.github.com>
2026-04-24 14:38:29 +0530

    Commit Object created

--- src/main/java/com/GitClone/Git/controller/GitController.java
+++ src/main/java/com/GitClone/Git/controller/GitController.java
@@ -1,4 +1,25 @@
 package com.GitClone.Git.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/git")
 public class GitController {
+
+    @PostMapping("/init")
+    public void gitInit()
+    {
+
+    }
+    @PostMapping("/add")
+    public void gitAdd(@RequestParam String path, @RequestBody String body)
+    {
+
+    }
+    @GetMapping("/log")
+    public void gitLog()
+    {
+
+    }
+
 }
--- src/main/java/com/GitClone/Git/model/Commit.java
+++ src/main/java/com/GitClone/Git/model/Commit.java
@@ -1,4 +1,45 @@
 package com.GitClone.Git.model;
 
-public class Commit {
+import lombok.Data;
+
+import java.nio.charset.StandardCharsets;
+
+
+import java.nio.charset.StandardCharsets;
+
+@Data
+public class Commit extends GitObject {
+
+    private String treeSha;
+    private String parentCommitSha; // null for first commit
+    private String message;
+    private String author;
+    private long timestamp;
+
+    public Commit(String treeSha, String parentCommitSha, String message, String author) {
+        this.setType("commit");
+        this.treeSha = treeSha;
+        this.parentCommitSha = parentCommitSha;
+        this.message = message;
+        this.author = author;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    @Override
+    public byte[] serialize() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("tree ").append(treeSha).append("\n");
+
+        if (parentCommitSha != null) {
+            sb.append("parent ").append(parentCommitSha).append("\n");
+        }
+
+        sb.append("author ").append(author).append("\n");
+        sb.append("timestamp ").append(timestamp).append("\n");
+        sb.append("\n"); // separator
+        sb.append(message).append("\n");
+
+        return sb.toString().getBytes(StandardCharsets.UTF_8);
+    }
 }
