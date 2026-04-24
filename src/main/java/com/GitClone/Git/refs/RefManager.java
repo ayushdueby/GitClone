22fbe8b0748d90e4f03ce930b900ebaaefeac843
ayushdueby <115997065+ayushdubey@users.noreply.github.com>
2026-04-24 16:16:04 +0530

    RefManager dataLogic added

--- src/main/java/com/GitClone/Git/refs/RefManager.java
+++ src/main/java/com/GitClone/Git/refs/RefManager.java
@@ -1,4 +1,39 @@
 package com.GitClone.Git.refs;
 
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
 public class RefManager {
+    public String headBranch;
+    public Map<String,String>latestCommitByBranch;
+
+    public RefManager()
+    {
+        this.latestCommitByBranch=new HashMap<>();
+    }
+    public String checkOut(String checkOutBranch)
+    {
+        if(!latestCommitByBranch.containsKey(checkOutBranch))
+        {
+            throw new RuntimeException("Branch does not exists");
+        }
+        headBranch=checkOutBranch;
+        return headBranch;
+    }
+    public void createBranch(String branchToAdd,String sha)
+    {
+        latestCommitByBranch.put(branchToAdd,sha);
+    }
+    public String getHeadSha()
+    {
+        return latestCommitByBranch.get(headBranch);
+    }
+    //uska latest sha return hoga and we will update that into our key branch's value
+    public void updateCurrentBranchAfterCommit(String newSha)
+    {
+        latestCommitByBranch.put(headBranch,newSha);
+    }
 }
